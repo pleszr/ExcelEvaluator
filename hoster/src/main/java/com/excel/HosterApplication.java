21b878cacdca17e69a91ca297d88246cf77c6966
pleszr <145920141+pleszr@users.noreply.github.com>
2024-01-28 22:01:17 +0100

    hoster base is built

---
+++ hoster/src/main/java/com/excel/HosterApplication.java
@@ -0,0 +1,13 @@
+package com.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosterApplication.class, args);
+	}
+
+}
